0cef79d50dbfc58657cbd040942eb36f4a5061d6
Kostya <kostantinkorocenko301@gmail.com>
2025-08-12 23:50:08 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,46 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { User } from './types/User';
+import { getTodos, getUser } from './api';
+import { useEffect } from 'react';
 
 export const App: React.FC = () => {
+  const [loadingUser, setLoadingUser] = React.useState(false);
+  const [todos, setTodos] = React.useState<Todo[]>([]);
+  const [loading, setLoading] = React.useState(true);
+  const [user, setUser] = React.useState<User | null>(null);
+  const [selectedTodo, setSelectedTodo] = React.useState<Todo | null>(null); // Added missing state
+
+  useEffect(() => {
+    getTodos()
+      .then(data => {
+        setTodos(data);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  const handleUserFetch = (userId: number) => {
+    setLoadingUser(true);
+    
+    // Find the todo by userId
+    const todo = todos.find(t => t.userId === userId);
+    if (todo) {
+      setSelectedTodo(todo);
+    }
+    
+    getUser(userId)
+      .then(data => {
+        setUser(data);
+      })
+      .finally(() => {
+        setLoadingUser(false);
+      });
+  };
+
   return (
     <>
       <div className="section">
@@ -21,14 +59,22 @@ export const App: React.FC = () => {
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {loading && <Loader />}
+              {!loading && <TodoList todos={todos} onTodoSelect={handleUserFetch} />}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      <TodoModal
+        selectedTodo={selectedTodo}
+        user={user}
+        loadingUser={loadingUser}
+        onClose={() => {
+          setSelectedTodo(null);
+          setUser(null);
+        }}
+      />
     </>
   );
 };
